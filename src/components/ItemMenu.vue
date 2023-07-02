0a08267cc524ce40d671b467e5ac78908c29c1bb
Miguel <miguel.gf.130@gmail.com>
2023-07-02 11:57:31 -0600

    [Common] Se quitan vistas de ejemplo y se agregan neuvas de home y operador

---
+++ src/components/ItemMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <router-link :to="props.ruta">
+    <el-menu-item :index="props.index">
+      <el-icon><component :is="props.icono" /></el-icon>
+      <template #title>
+        {{ props.titulo || "--" }}
+      </template>
+    </el-menu-item>
+  </router-link>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  index: {
+    type: String,
+    default: "0",
+  },
+  titulo: {
+    type: String,
+    default: "",
+  },
+  icono: {
+    required: true,
+  },
+  ruta: {
+    type: String,
+    default: "/",
+  },
+});
+</script>
---
+++ src/views/OperadorView.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-container> Operadores </el-container>
+</template>
+
+<script>
+export default {
+  name: "OperadorView",
+};
+</script>
