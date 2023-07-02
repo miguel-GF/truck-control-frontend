05080d3a910b12d4e69b088f20aa5ad2fdcfbfad
Miguel <miguel.gf.130@gmail.com>
2023-07-02 11:58:44 -0600

    [Config] Se actulizan rutas e import de iconos y hora local spanish

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import es from "element-plus/dist/locale/es.mjs";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(ElementPlus, {
+  locale: es,
+});
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.use(router);
+app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: import(/* webpackChunkName: "home" */ "../views/HomeView.vue"),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/home",
+    name: "home",
+    component: import("../views/HomeView.vue"),
+  },
+  {
+    path: "/operadores",
+    name: "operadores",
+    component: () => import("../views/OperadorView.vue"),
   },
 ];
 
