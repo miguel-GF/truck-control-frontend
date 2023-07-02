8b82ad0472abf6bb7969240aed2c82bb7d1298df
Miguel <miguel.gf.130@gmail.com>
2023-07-02 11:59:14 -0600

    [App] Se modifia app view inicial para tener menu ehader y footer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,110 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <el-container class="layout">
+    <el-container>
+      <!-- <el-aside class="el-aside" width="200px">Aside</el-aside> -->
+      <el-menu
+        default-active="0"
+        class="el-menu-vertical-demo"
+        :collapse="true"
+      >
+        <!--<el-sub-menu index="1">
+          <template #title>
+            <el-icon><UserFilled /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group>
+            <template #title><span>Group One</span></template>
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title><span>item four</span></template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu> -->
+        <template v-for="(opc, i) in menuOpciones" :key="i">
+          <ItemMenu
+            :titulo="opc.titulo"
+            :index="String(i)"
+            :icono="opc.icono"
+            :ruta="opc.ruta"
+          />
+        </template>
+      </el-menu>
+      <el-drawer v-model="drawer" title="Menu" direction="ltr" size="20%">
+        <div class="cursor-pointer">
+          <el-row justify="space-between">
+            <el-col :span="4">
+              <el-icon>
+                <UserFilled />
+              </el-icon>
+            </el-col>
+            <el-col :span="20">2</el-col>
+          </el-row>
+        </div>
+      </el-drawer>
+      <el-container>
+        <el-header class="el-header">
+          <el-row justify="space-between">
+            <el-icon @click="drawer = !drawer" class="cursor-pointer">
+              <Expand />
+            </el-icon>
+            <el-icon class="cursor-pointer">
+              <More />
+            </el-icon>
+          </el-row>
+        </el-header>
+        <el-main>
+          <router-view />
+        </el-main>
+        <el-footer class="el-footer d-end" height="24px">
+          <div>Footer 2023</div>
+        </el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup>
+import { ref, shallowRef } from "vue";
+import { ElIcon } from "element-plus";
+import { HomeFilled, UserFilled } from "@element-plus/icons-vue";
+import ItemMenu from "./components/ItemMenu.vue";
+const drawer = ref(false);
+const userFilledRef = shallowRef(UserFilled);
+const homeFilledRef = shallowRef(HomeFilled);
+const menuOpciones = ref([
+  { titulo: "Home", icono: homeFilledRef, ruta: "/" },
+  { titulo: "Operadores", icono: userFilledRef, ruta: "/operadores" },
+]);
+</script>
+
+<style lang="scss" #scope>
+// .el-aside {
+//   background-color: $background;
+// }
+
+.el-footer {
+  background-color: $background;
 }
 
-nav {
-  padding: 30px;
+.el-header {
+  background-color: $background;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 50px !important;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+/* Distribuye el espacio restante horizontalmente */
+.el-header .el-row {
+  flex-grow: 1;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.layout {
+  height: 100vh;
 }
 </style>
