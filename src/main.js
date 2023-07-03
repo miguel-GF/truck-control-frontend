0243f41a24502ebcf973d0ac104ab7653b718f88
Miguel <miguel.gf.130@gmail.com>
2023-07-02 19:11:41 -0600

    [Estilos] Se agregan nuevos estilos y se importan display.css de element plus

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/display.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import es from "element-plus/dist/locale/es.mjs";
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: import(/* webpackChunkName: "home" */ "../views/HomeView.vue"),
-  },
-  {
     path: "/home",
     name: "home",
-    component: import("../views/HomeView.vue"),
+    component: () =>
+      import(/* webpackChunkName: "home" */ "../views/HomeView.vue"),
   },
   {
     path: "/operadores",
