74e503baa2df92604b315c039cb302b04eae3d98
Miguel <miguel.gf.130@gmail.com>
2023-07-09 15:54:12 -0600

    [GlobalProperties] Se agrega método para mostrar mensajes en pantalla

---
+++ src/globalProperties/message.js
@@ -0,0 +1,26 @@
+import { ElMessage } from "element-plus";
+
+const obtenerTipo = (tipo) => {
+  switch (tipo) {
+    case "exito":
+    case "success":
+      return "success";
+    case "advertencia":
+    case "warning":
+      return "warning";
+    case "error":
+      return "error";
+    default:
+      return "info";
+  }
+};
+
+// Método para mostrar mensajes de Exito/Error etc
+export const mostrarMensaje = (mensaje, tipo = "exito") => {
+  return ElMessage({
+    message: mensaje,
+    type: obtenerTipo(tipo),
+    showClose: true,
+    grouping: true,
+  });
+};
