4c7cfef60622d44aaeca2309d50e716313dfbf9c
Miguel <miguel.gf.130@gmail.com>
2023-07-12 19:38:18 -0600

    [Config] Se agrega librerias para configurar vue js respecto a element plus

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
@@ -7,5 +10,22 @@ module.exports = defineConfig({
         additionalData: `@import "@/css/estilos.scss";`,
       },
     },
+  },
+  devServer: {
+    client: {
+      overlay: {
+        runtimeErrors: false,
+      },
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
   },
 });
