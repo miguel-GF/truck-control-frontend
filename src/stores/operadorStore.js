5efbd0fc808a5544da776489b208f6fbe28a099b
Miguel <miguel.gf.130@gmail.com>
2023-07-12 19:41:50 -0600

    [Store][Operadores] Se agrega data y método para obtener filtrados el listado de operadores

--- src/stores/operadorStore.js
+++ src/stores/operadorStore.js
@@ -1,19 +1,32 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref } from "@/importsVue";
 import { get } from "../api/api";
 
 export const useOperadorStore = defineStore("operadores", () => {
   const operadores = ref([]);
+  const filtradosOperadores = ref([]);
   const listar = async () => {
     try {
       const { datos } = await get("operadores");
       operadores.value = datos;
+      filtradosOperadores.value = datos;
     } catch (error) {
       throw new Error(error);
     }
   };
+  const filtrarListar = async (busqueda) =>
+    (filtradosOperadores.value = operadores.value.filter(
+      (data) =>
+        !busqueda.value ||
+        data.nombre_operador
+          .toLowerCase()
+          .includes(busqueda.value.toLowerCase()) ||
+        String(data.clave).toLowerCase().includes(busqueda.value.toLowerCase())
+    ));
   return {
     operadores,
+    filtradosOperadores,
     listar,
+    filtrarListar,
   };
 });
