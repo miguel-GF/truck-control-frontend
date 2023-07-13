4021072b7309cef0f5087b703ed0c8ad14e81589
Miguel <miguel.gf.130@gmail.com>
2023-07-12 19:42:33 -0600

    [Operadores] Se actualiza diseño y lógica para que convivan por refactorización de componentes

--- src/views/operadores/OperadorList.vue
+++ src/views/operadores/OperadorList.vue
@@ -4,9 +4,35 @@ import { storeToRefs } from "pinia";
 import { onBeforeMount, inject, ref } from "@/importsVue";
 
 const useOperadores = useOperadorStore();
-const { operadores } = storeToRefs(useOperadores);
+const { filtradosOperadores } = storeToRefs(useOperadores);
 const { listar } = useOperadores;
 const showLoading = inject("$showLoading");
+const columnas = ref([
+  {
+    id: "clave",
+    prop: "clave",
+    label: "Clave",
+    sortable: true,
+    formatter: (row) => row.clave ?? "--",
+  },
+  {
+    id: "nombre",
+    prop: "nombre_operador",
+    label: "Nombre",
+    sortable: true,
+    formatter: (row) => row.nombre_operador ?? "--",
+  },
+  {
+    id: "telefono",
+    prop: "telefono",
+    label: "Teléfono",
+    sortable: false,
+    formatter: (row) => row.telefono ?? "--",
+  },
+  {
+    id: "opciones",
+  },
+]);
 
 onBeforeMount(async () => {
   try {
@@ -21,9 +47,27 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <template v-for="operador in operadores" :key="operador.id">
-    <el-row :span="24">
-      {{ operador.nombre || "--" }}
-    </el-row>
-  </template>
+  <el-table
+    :data="filtradosOperadores"
+    stripe
+    table-layout="fixed"
+    class="tabla-gestor"
+  >
+    <el-table-column
+      v-for="columna in columnas"
+      :key="columna.id"
+      :prop="columna.prop ?? ''"
+      :label="columna.label ?? ''"
+      :sortable="columna.sortable ?? false"
+      :formatter="columna.formatter ?? (() => {})"
+    >
+      <!-- <template v-if="columna.id == 'opciones'" #header>
+        <el-input
+          v-model="busqueda"
+          size="small"
+          placeholder="Type to search"
+        />
+      </template> -->
+    </el-table-column>
+  </el-table>
 </template>
--- src/views/operadores/OperadorView.vue
+++ src/views/operadores/OperadorView.vue
@@ -1,19 +1,30 @@
 <template>
   <el-container>
-    Operadores
-    <br />
-    <el-container>
-      <OperadorList />
-    </el-container>
+    <el-row>
+      <TheHeader
+        class="header-view"
+        placeholder="Buscar por Clave / Nombre"
+        @buscar="filtrar"
+        @agregar="agregar()"
+      />
+      <!-- <el-scrollbar style="height: calc(100vh - 460px)"> -->
+      <!-- Ajusta la altura según tus necesidades -->
+      <el-col :span="24">
+        <OperadorList />
+      </el-col>
+      <!-- </el-scrollbar> -->
+    </el-row>
   </el-container>
 </template>
 
-<script>
+<script setup>
 import OperadorList from "./OperadorList.vue";
-export default {
-  name: "OperadorView",
-  components: {
-    OperadorList,
-  },
+import TheHeader from "../../components/TheHeader.vue";
+import { useOperadorStore } from "@/stores/operadorStore.js";
+const useOperadores = useOperadorStore();
+const { filtrarListar } = useOperadores;
+const filtrar = async (busqueda) => {
+  await filtrarListar(busqueda);
 };
+const agregar = () => console.log("agregar");
 </script>
