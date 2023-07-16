e9ea4e066965762c7059d24ce46b86e91d0b16fc
miguel-GF <miguel.gf.130@gmail.com>
2023-07-16 08:24:43 -0600

    [Store][Operador] Se agrega método para editar

--- src/stores/operadorStore.js
+++ src/stores/operadorStore.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "@/importsVue";
-import { get, post } from "../api/api";
+import { get, post, patch } from "../api/api";
 
 export const useOperadorStore = defineStore("operadores", () => {
   const operadores = ref([]);
@@ -32,11 +32,29 @@ export const useOperadorStore = defineStore("operadores", () => {
       throw new Error(error);
     }
   };
+  const editar = async (datosEditar) => {
+    try {
+      const respuesta = await patch("operadores", datosEditar);
+      const { datos } = respuesta;
+      const index = operadores.value.findIndex((o) => o.id == datos.id);
+      if (index >= 0) {
+        operadores.value[index].nombre_operador = datos.nombre_operador;
+        operadores.value[index].nombre = datos.nombre;
+        operadores.value[index].apellidos = datos.apellidos;
+        operadores.value[index].telefono = datos.telefono;
+      }
+      operadores.value.push(respuesta.datos);
+      return respuesta;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
   return {
     operadores,
     filtradosOperadores,
     listar,
     filtrarListar,
     agregar,
+    editar,
   };
 });
