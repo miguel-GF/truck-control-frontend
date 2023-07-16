0c6801e54f72327c8cbb6d070769383d58851325
Miguel <miguel.gf.130@gmail.com>
2023-07-16 06:06:19 -0600

    [Store][App] Se agrega variable global para manejar el estado del btn de aceptar para bloquearse y mostrar su loader

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -4,8 +4,12 @@ import { ref } from "@/importsVue";
 export const useAppStore = defineStore("app", () => {
   const isMobile = ref(false);
   const setIsMobile = async (value) => (isMobile.value = value);
+  const btnLoader = ref(false);
+  const changeBtnLoader = async () => (btnLoader.value = !btnLoader.value);
   return {
     isMobile,
     setIsMobile,
+    btnLoader,
+    changeBtnLoader,
   };
 });
