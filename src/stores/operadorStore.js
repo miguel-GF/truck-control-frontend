bf20763af0200104958a197a7610c8a5bdc4cdb9
Miguel <miguel.gf.130@gmail.com>
2023-07-20 20:06:19 -0600

    [Store][Operador] Se agrega método de eliminar y actualización de validación de busqueda id para editar

--- src/stores/operadorStore.js
+++ src/stores/operadorStore.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "@/importsVue";
-import { get, post, patch } from "../api/api";
+import { get, post, patch, del } from "../api/api";
 
 export const useOperadorStore = defineStore("operadores", () => {
   const operadores = ref([]);
@@ -36,12 +36,30 @@ export const useOperadorStore = defineStore("operadores", () => {
     try {
       const respuesta = await patch("operadores", datosEditar);
       const { datos } = respuesta;
-      const index = operadores.value.findIndex((o) => o.id == datos.id);
+      const index = operadores.value.findIndex(
+        (o) => Number(o.id) === Number(datos.id)
+      );
       if (index >= 0) {
         operadores.value[index].nombre_operador = datos.nombre_operador;
         operadores.value[index].nombre = datos.nombre;
         operadores.value[index].apellidos = datos.apellidos;
         operadores.value[index].telefono = datos.telefono;
+      }
+      return respuesta;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  const eliminar = async (datosEditar) => {
+    try {
+      const respuesta = await del("operadores", datosEditar);
+      const { datos } = respuesta;
+      const index = operadores.value.findIndex(
+        (o) => Number(o.id) === Number(datos.id)
+      );
+      if (index >= 0) {
+        operadores.value[index].status = datos.status;
+        operadores.value[index].status_nombre = datos.status_nombre;
       }
       return respuesta;
     } catch (error) {
@@ -55,5 +73,6 @@ export const useOperadorStore = defineStore("operadores", () => {
     filtrarListar,
     agregar,
     editar,
+    eliminar,
   };
 });
